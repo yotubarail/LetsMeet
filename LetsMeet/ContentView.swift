cfca3b7cdae4bed0b444010ae19dae66cba3bcec
Shota Takinami <shota_t@i.softbank.jp>
2020-08-25 17:05:24 +0900

    Initial Commit

---
+++ LetsMeet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LetsMeet
+//
+//  Created by 滝浪翔太 on 2020/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
